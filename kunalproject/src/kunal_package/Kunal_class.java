3ff2397d18aca3d8c5d3d49c3336841f5963f43b
kunal-patil5999 <127413584+kunal-patil5999@users.noreply.github.com>
2023-03-09 22:28:25 +0530

    First commit

---
+++ kunalproject/src/kunal_package/Kunal_class.java
@@ -0,0 +1,14 @@
+package kunal_package;
+
+public class Kunal_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("task complete");
+        System.out.println("task incomplete");
+        System.out.println("task progess");
+        System.out.println("task done");
+        
+	}
+
+}
